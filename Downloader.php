0eceb1996b8fd3810a04de91366f56febfd46974
Jeff Lee @ N61 <leejefon@gmail.com>
2013-05-14 13:59:07 -0400

    Added zip support + updated README

--- Downloader.php
+++ Downloader.php
@@ -37,6 +37,8 @@ class Downloader extends Download {
 
 		$file_path = $this->getFile($this->uri, $this->files);
 
+		$file_name = $this->encodeFilename(basename($file_path));
+
 		if ($file_path == null) {
 			echo "File not found";;
 		} else {
@@ -45,13 +47,16 @@ class Downloader extends Download {
 
 		if (isset($this->options["zip"]) && $this->options["zip"] == true) {
 			// check already zipped
-			// do zip
+			
+			$file_name .= ".zip";
+			$this->createZip(array($file_path), $file_name);
+			$file_path = $file_name;
 		}
 
 		if (file_exists($file_path)) {
 			header('Content-Description: File Transfer');
 			header('Content-Type: ' . mime_content_type($file_path));
-			header('Content-Disposition: attachment; filename=' . $this->encodeFilename(basename($file_path)));
+			header('Content-Disposition: attachment; filename=' . $file_name);
 			header('Content-Transfer-Encoding: binary');
 			header('Expires: 0');
 			header("Cache-Control: must-revalidate, post-check=0, pre-check=0"); 
@@ -64,13 +69,18 @@ class Downloader extends Download {
 		}
 
 		if (isset($this->options["zip"]) && $this->options["zip"] == true) {
-			// unlink file
+			unlink($file_path);
 		}
 
 		exit;
 	}
 
 	private function getFile($uri, $files) {
+		if ($this->endsWith($uri[count($uri) - 1], '.zip')) {
+			$this->options['zip'] = true;
+			$uri[count($uri) - 1] = str_replace(".zip", "", $uri[count($uri) - 1]);
+		}
+
 		foreach ($files as $key => $val) {
 			if ($uri[1] == $key) {
 				if (is_array($val)) {
@@ -89,6 +99,42 @@ class Downloader extends Download {
 		return str_replace(" ", "_", $file);
 	}
 
+	private function endsWith($haystack, $needle) {
+		$length = strlen($needle);
+		if ($length == 0) {
+			return true;
+		}
+
+		return (substr($haystack, -$length) === $needle);
+	}
+
+	private function getAuth() {
+		$this->options = $this->getOptions();
+		if ($this->options != null) {
+			if (isset($this->options["auth"])) {
+				return $this->auth[$this->options["auth"]];
+			}
+		}
+		return null;
+	}
+
+	private function getOptions() {
+		$level = count($this->uri);
+		$options = $this->options;
+
+		for ($i = 1; $i < $level; $i++) {
+			if (isset($options[$this->uri[$i]])) {
+				$options = $options[$this->uri[$i]];
+			}
+		}
+
+		if ($i == $level) {
+			return $options;
+		} else {
+			return null;
+		}
+	}
+
 	private function authenticate() {
 		$auth = $this->getAuth();
 
@@ -103,13 +149,8 @@ class Downloader extends Download {
 			exit;
 		}
 
-		$username = $_SERVER["PHP_AUTH_USER"]; 
-		if (!empty($auth['salt'])) {
-			$password = sprintf($auth['salt'], $_SERVER['PHP_AUTH_PW']);
-		} else {
-			$password = $_SERVER['PHP_AUTH_PW'];
-		}
-		$password = call_user_func($auth["hash"], $password);
+		$username = $_SERVER["PHP_AUTH_USER"]; // handle hash and salt
+		$password = call_user_func($auth["hash"], $auth["salt"] . $_SERVER["PHP_AUTH_PW"]);
 
 		$type = explode("/", $auth["type"]);
 
@@ -147,30 +188,45 @@ class Downloader extends Download {
 		return;
 	}
 
-	private function getAuth() {
-		$this->options = $this->getOptions();
-		if ($this->options != null) {
-			if (isset($this->options["auth"])) {
-				return $this->auth[$this->options["auth"]];
+	// Source: http://davidwalsh.name/create-zip-php
+	function createZip($files = array(), $destination = '', $overwrite = false) {
+		//if the zip file already exists and overwrite is false, return false
+		if (file_exists($destination) && !$overwrite) { return false; }
+		//vars
+		$valid_files = array();
+
+		//if files were passed in...
+		if (is_array($files)) {
+			//cycle through each file
+			foreach ($files as $file) {
+				//make sure the file exists
+				if (file_exists($file)) {
+					$valid_files[] = $file;
+				}
 			}
 		}
-		return null;
-	}
 
-	private function getOptions() {
-		$level = count($this->uri);
-		$options = $this->options;
-
-		for ($i = 1; $i < $level; $i++) {
-			if (isset($options[$this->uri[$i]])) {
-				$options = $options[$this->uri[$i]];
+		//if we have good files...
+		if (count($valid_files)) {
+			//create the archive
+			$zip = new ZipArchive();
+			if ($zip->open($destination, $overwrite ? ZIPARCHIVE::OVERWRITE : ZIPARCHIVE::CREATE) !== true) {
+				return false;
 			}
-		}
-
-		if ($i == $level) {
-			return $options;
+			//add the files
+			foreach ($valid_files as $file) {
+				$zip->addFile($file, basename($file));
+			}
+			//debug
+			//echo 'The zip archive contains ',$zip->numFiles,' files with a status of ',$zip->status;
+		
+			//close the zip -- done!
+			$zip->close();
+		
+			//check to make sure the file exists
+			return file_exists($destination);
 		} else {
-			return null;
+			return false;
 		}
 	}
 }
--- README.md
+++ README.md
@@ -1,4 +1,29 @@
 Simple-PHP-Downloader
 =====================
 
-Simple PHP Downloader
+Example Usage:
+--------------
+
+1. Extract the files in /downloader
+2. Update .htaccess accordingly
+3. Enter paths of files to download in Download.php
+
+
+Options:
+--------
+auth => auth name in $auth
+zip* => true/false
+
+* Files can be downloaded zipped by appending .zip to the end of download path as well.
+
+
+Auth:
+-----
+realm => Display name for auth popup
+type => database/mysql
+dbname => Database name
+table => Table name
+user => User login
+pass => User password
+hash => md5, sha1, or whatever hash function used to encrypt the password.
+salt => Salt used in password.  Right now it only prepends to password.  If salt is attached differently, modify it in Downloader->authenticate function.
